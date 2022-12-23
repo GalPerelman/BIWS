136cd5607d490c02d636b02c03bd221cbac59632
GalPerelman <peligur@gmail.com>
2022-12-23 13:10:20 +0200

    refactor to exhaustive search module

--- exhaustive.py
+++ exhaustive.py
@@ -20,6 +20,10 @@ RESOURCES_DIR = os.path.join(BASE_DIR, 'resources')
 
 
 class ControlChecker:
+    """
+    inp must be cleaned of previous controls
+
+    """
     def __init__(self, inp_path, zone, valves, output_dir):
         self.inp_path = inp_path
         self.zone = zone
@@ -27,30 +31,42 @@ class ControlChecker:
         self.output_dir = output_dir
 
         self.inp_name = utils.get_file_name_from_path(self.inp_path)[0]
-        self.output_dir = utils.validate_dir_path(os.path.join(self.output_dir, self.inp_name))
+        self.output_dir = utils.validate_dir_path(os.path.join(self.output_dir))
         self.wn = wntr.network.WaterNetworkModel(self.inp_path)
+        self.wn.options.time.duration = 48*3600
+
         self.benchmark = Evaluator([self.wn]).evaluate_scenario()
+        self.results = {}
 
-    def evaluate_control(self):
-        self.control_perf = {}
-        perf, total_demand, total_supply = self.evaluate(self.wn)
-        self.control_perf['All closed'] = perf
-        self.control_perf['All closed']['norm'] = 9
-        self.control_perf['All closed']['total demand'] = total_demand
-        self.control_perf['All closed']['total supply'] = total_supply
+    def set_all_elements(self, net, status: str):
+        for v_name in self.valves:
+            net.get_link(v_name).initial_status = status
+        return net
 
-        valves_two_settings = [
-            (valve, time)
-            for valve in self.valves
-            for time in ['night', 'morning']
-        ]
+    def record_results(self, desc, metrics, normalized, total_demand, total_supply):
+        self.results[desc] = metrics
+        self.results[desc]['normalized'] = normalized
+        self.results[desc]['total_demand'] = total_demand
+        self.results[desc]['total_supply'] = total_supply
+
+    def evaluate_controls(self):
+        # all closed
+        net = deepcopy(self.wn)
+        self.set_all_elements(net, 'Closed')
+        metrics, total_demand, total_supply = evaluate(self.wn)
+        norm = sum(utils.normalize_obj(self.benchmark, metrics).values())
+        self.record_results('all_closed', metrics, norm, total_demand, total_supply)
+
+        # iterate all combinations
+        valves_two_settings = [(valve, time) for valve in self.valves for time in ['night', 'morning']]
+
         for n in range(1, len(valves_two_settings)):
-            print('analyzing %i pipe open' % n)
+            print(f'analyzing {n} pipe open\n')
 
             for cvs in tqdm(list(combinations(valves_two_settings, n))):
                 net = deepcopy(self.wn)
-                # check if both night and morning are open
-                # if yes, no need to add rule
+
+                # check if both night and morning are open, if yes, no need to add rule
                 cv_list = [cv for (cv, _) in cvs]
                 cv_count = Counter(cv_list)
                 all_open_cv = [cv for cv, count in cv_count.items() if count == 2]
@@ -60,34 +76,23 @@ class ControlChecker:
                 for (cv, time) in cvs:
                     if cv not in all_open_cv:
                         net = self.add_control(net, net.get_link(cv), time)
-                perf, total_demand, total_supply = self.evaluate(net)
-                self.control_perf[cvs] = perf
-                so = sum(utils.normalize_obj(self.benchmark, perf).values())
-                self.control_perf[cvs]['norm'] = so
-                self.control_perf[cvs]['total demand'] = total_demand
-                self.control_perf[cvs]['total supply'] = total_supply
+                metrics, total_demand, total_supply = evaluate(net)
+                norm = sum(utils.normalize_obj(self.benchmark, metrics).values())
+                self.record_results(cvs, metrics, norm, total_demand, total_supply)
 
-        # open all pipes
+        # all opened
         net = deepcopy(self.wn)
-        for cv in self.valves:
-            net.get_link(cv).initial_status = 'Open'
-        perf, total_demand, total_supply = self.evaluate(net)
-        self.control_perf['All open'] = perf
-        so = sum(utils.normalize_obj(self.benchmark, perf).values())
-        self.control_perf['All open']['norm'] = so
-        self.control_perf['All open']['total demand'] = total_demand
-        self.control_perf['All open']['total supply'] = total_supply
-        self.control_perf = pd.DataFrame(self.control_perf)
-        self.control_perf.T.to_csv(os.path.join(self.output_dir, self.zone + '_valve_control.csv'))
+        self.set_all_elements(net, 'Open')
+        metrics, total_demand, total_supply = evaluate(self.wn)
+        norm = sum(utils.normalize_obj(self.benchmark, metrics).values())
+        self.record_results('all_open', metrics, norm, total_demand, total_supply)
 
-    def evaluate(self, net):
-        eval = Evaluator([net])
-        return eval.evaluate_scenario(), eval.get_total_demand(), eval.get_total_supply()
+        self.export()
 
     def add_control(self, net, elem, time):
         cond_beginning = controls.TimeOfDayCondition( net, relation='=', threshold=0)
-        cond = controls.TimeOfDayCondition(net, relation='=', threshold=7 * 3600)
-        cond_end = controls.TimeOfDayCondition(net, relation='=', threshold=24 * 3600)
+        cond = controls.TimeOfDayCondition(net, relation='=', threshold=7*3600)
+        cond_end = controls.TimeOfDayCondition(net, relation='=', threshold=24*3600)
         if time == 'night':
             night_close = controls.Control(cond, controls.ControlAction(elem, 'status', 0))
             night_open = controls.Control(cond_beginning, controls.ControlAction(elem, 'status', 1))
@@ -101,6 +106,18 @@ class ControlChecker:
             net.add_control('control_morning_close' + str(elem.name), morning_close)
 
         return net
+
+    def export(self):
+        results = utils.handle_dict(self.results)
+        results = pd.DataFrame(results)
+        results.T.to_csv(os.path.join(self.output_dir, self.zone + '_valve_control.csv'))
+        # wntr.network.io.write_inpfile(net, os.path.join(self.output_dir, self.zone + results.T.index[-1] + '.inp'))
+
+
+def evaluate(net):
+    evaluator = Evaluator([net])
+    metrics = utils.round_dict(evaluator.evaluate_scenario(), 4)
+    return metrics, evaluator.get_total_demand(), evaluator.get_total_supply()
 
 
 def iterate_all_pumps_combs(networks_path: str, export_path):
@@ -145,13 +162,13 @@ def replace_pumps_and_evaluate_solution(networks_path, pumps_to_replace: pd.Data
 
 
 if __name__ == '__main__':
-    inp_file_path = os.path.join(RESOURCES_DIR, 'networks', 'Input', 'y1.inp')
+    inp_file_path = os.path.join(RESOURCES_DIR, 'networks', 'Greedy_output', 'y1-no-controls.inp')
     elements_file_path = os.path.join(RESOURCES_DIR, 'valves.json')
-    output_path = os.path.join(BASE_DIR, 'output', 'controls')
+    output_path = os.path.join(BASE_DIR, 'output', 'controls', 'test')
     with open(elements_file_path) as f:
         grouped_elements = json.load(f)
 
         grouped_elements = {'class1': grouped_elements['class1']}
         for group, elements in grouped_elements.items():
             cc = ControlChecker(inp_file_path, group, elements, output_path)
-            cc.evaluate_control()
+            cc.evaluate_controls()
